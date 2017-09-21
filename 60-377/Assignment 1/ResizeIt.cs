5d30dd03c0759bb65cb576147f9ada494b931828
Ostoic <dinnertable@hotmail.ca>
2017-09-21 00:11:30 -0400

    Finished resize and rotation scripts

--- 60-377/Assignment 1/ResizeIt.cs
+++ 60-377/Assignment 1/ResizeIt.cs
@@ -2,17 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResizeIt : MonoBehaviour {
+public class ResizeIt : MonoBehaviour
+{
+    public float speed = 1.0f;
 
-	// Use this for initialization
-	void Start ()
-    {
+    Vector3 largeScale;
+    Vector3 baseScale;
 
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    Vector3 targetScale;
+    Vector3 nextScale;
+
+    // Generic swap
+    static void Swap<T>(ref T lhs, ref T rhs)
     {
-		
-	}
+        T temp = lhs;
+        lhs = rhs;
+        rhs = temp;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        baseScale = transform.localScale;
+
+        largeScale = baseScale * 4;
+        targetScale = largeScale;
+        nextScale = baseScale;
+    }
+
+    void update_target_scale()
+    {
+        if (transform.localScale == targetScale)
+        {
+            Swap<Vector3>(ref targetScale, ref nextScale);
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        update_target_scale();
+
+        transform.localScale =
+            Vector3.MoveTowards(transform.localScale, targetScale, speed * Time.deltaTime);
+    }
 }
--- 60-377/Assignment 1/RotateIt.cs
+++ 60-377/Assignment 1/RotateIt.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class RotateIt : MonoBehaviour {
 
+    public Vector3 speed = new Vector3(30, 60, 90);
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        transform.Rotate(speed * Time.deltaTime);
+        //transform.eulerAngles += speed * Time.deltaTime;
 	}
 }
