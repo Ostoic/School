01aa5b4d799eca7c84d52eddac6aa5768106520d
Ostoic <dinnertable@hotmail.ca>
2017-11-28 18:22:47 -0500

    Added charges to spells.

--- 60-377/Major Project/Scripts/Spells/Spell.cs
+++ 60-377/Major Project/Scripts/Spells/Spell.cs
@@ -10,6 +10,8 @@ namespace Spells
 {
     public abstract class Spell
     {
+        private enum ChargeType { Infinite = -1, Finite = 0 };
+
         private int manaCost;
         private float range;
 
@@ -20,15 +22,21 @@ namespace Spells
 
         protected Unit caster;
 
-        public Spell(Unit caster, float range, int manaCost, float cooldown)
+        public Spell(Unit caster, float range, int manaCost, int charges, float cooldown)
         {
             this.manaCost = manaCost;
             this.cooldown = cooldown;
+            this.chargesLeft = charges;
             this.caster = caster;
             this.range = range;
         }
+
+        public Spell(Unit caster, float range, int manaCost, int charges)
+            : this(caster, range, manaCost, charges, 1.0f)
+        { }
+
         public Spell(Unit caster, float range, int manaCost)
-            : this(caster, range, manaCost, 1.0f)
+            : this(caster, range, manaCost, (int)ChargeType.Infinite)
         { }
 
         public Spell(Unit caster, float range)
@@ -47,6 +55,34 @@ namespace Spells
         public float GetRange()
         {
             return this.range;
+        }
+
+        /// <summary>
+        /// Set the number of charges the spell can use.
+        /// </summary>
+        /// <param name="charges">the number of charges.</param>
+        protected void SetCharges(int charges)
+        {
+            this.chargesLeft = charges;
+        }
+
+        /// <summary>
+        /// Determine if the spell has any charges left.
+        /// </summary>
+        /// <returns>true if the spell has charges left, false otherwise.</returns>
+        public bool HasCharges()
+        {
+            return this.chargesLeft > 0;
+        }
+
+        /// <summary>
+        /// Use a spell's charge and decrease the number of charges it has left.
+        /// </summary>
+        private void UseCharge()
+        {
+            // If the spell has a finite number of charges, decrease its number of charges left.
+            if (this.chargesLeft != (int)ChargeType.Infinite) 
+                this.chargesLeft--;
         }
 
         /// <summary>
@@ -86,14 +122,15 @@ namespace Spells
         }
 
         /// <summary>
-        ///  Cast a spell on the given target.
+        /// Cast a spell on the given target.
         /// </summary>
         /// <param name="target">The unit to cast the spell on.</param>
         /// <returns>false if the spell is on cooldown, true otherwise.</returns>
         public virtual bool Cast(Unit target)
         {
-            if (!this.IsOnCooldown())
+            if (this.HasCharges() && !this.IsOnCooldown())
             {
+                this.UseCharge();
                 this.castEpoch = Time.time;
                 return true;
             }
